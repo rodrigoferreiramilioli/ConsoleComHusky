57ea931e27538ba0ba3e97591756962bce5b25f4
Rodrigo Ferreira <rodrigosf@ciandt.com>
2023-01-12 08:02:31 -0300

    testessss

---
+++ TestUnit/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace TestUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var teste = false;
+            Assert.True(teste);
+        }
+    }
+}
